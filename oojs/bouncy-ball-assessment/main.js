fcdb774f9d6518bcdc9d029202ff5a0ef61dc26e
jsahlsa <joe.sahlstrom@gmail.com>
2020-10-07 22:03:27 +0000

    added first steps: shape constructor, changed Ball to inherit from Shape, added exists to new ball

---
+++ oojs/bouncy-ball-assessment/main.js
@@ -0,0 +1,113 @@
+// setup canvas
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+// function to generate random number
+
+function random(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num;
+}
+
+// shape class creation
+
+function Shape(x, y, velX, velY, exists) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.exists = exists;
+}
+
+// ball constructor properties
+
+function Ball(x, y, velX, velY, exists, color, size) {
+    Shape.call(this, x, y, velX, velY, exists);
+
+    this.color = color;
+    this.size = size;
+}
+
+// method for drawing the balls
+Ball.prototype.draw = function () {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
+// update method
+Ball.prototype.update = function () {
+    if ((this.x + this.size) >= width) {
+        this.velX = -(this.velX);
+    }
+    if ((this.x - this.size) <= 0) {
+        this.velX = -(this.velX);
+    }
+    if ((this.y + this.size) >= height) {
+        this.velY = -(this.velY);
+    }
+    if ((this.y - this.size) <= 0) {
+        this.velY = -(this.velY);
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+}
+
+// collision detect
+// loop, first checks that it doesn't collide with itself
+// then sets constants and if statement that test for a collision
+// if there is a collision, both balls change color
+Ball.prototype.collisionDetect = function () {
+    for (let j = 0; j < balls.length; j++) {
+        if (!(this === balls[j])) {
+            const dx = this.x - balls[j].x;
+            const dy = this.y - balls[j].y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < this.size + balls[j].size) {
+                balls[j].color = this.color = `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
+            }
+        }
+    }
+}
+
+// builds balls 
+const numberOfBalls = 30;
+
+let balls = [];
+
+while (balls.length < numberOfBalls) {
+    let size = random(10, 20);
+    let ball = new Ball(
+        random(0 + size, width - size),
+        random(0 + size, height - size),
+        random(-7, 7),
+        random(-7, 7),
+        true,
+        `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`,
+        size
+    );
+
+    balls.push(ball);
+}
+
+function loop() {
+    ctx.fillStyle = 'rgb(0, 0, 0, 0.5)';
+    ctx.fillRect(0, 0, width, height);
+
+    for (let i = 0; i < balls.length; i++) {
+        balls[i].draw();
+        balls[i].update();
+        balls[i].collisionDetect();
+    }
+
+    requestAnimationFrame(loop);
+}
+
+loop();
